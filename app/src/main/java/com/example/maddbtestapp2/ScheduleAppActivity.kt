14e49bb3d6c4ea390fa11d2179a05851092a384a
paula <276242@student.pwr.edu.pl>
2024-05-08 16:01:23 +0200

    Initial commit

---
+++ app/src/main/java/com/example/maddbtestapp2/ScheduleAppActivity.kt
@@ -0,0 +1,11 @@
+package com.example.maddbtestapp2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScheduleAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schedule_app)
+    }
+}
---
+++ app/src/main/java/com/example/maddbtestapp2/auth/firestore/FireStoreClass.kt
@@ -0,0 +1,24 @@
+package com.example.myapplicationmad.firestore
+
+import com.example.maddbtestapp2.auth.RegisterActivity
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
+
+class FireStoreClass {
+
+    private val mFireStore = FirebaseFirestore.getInstance()
+
+    fun registerUserFS (activity: RegisterActivity, userInfo: User){
+
+        mFireStore.collection("users")
+            .document(userInfo.id)
+            .set(userInfo, SetOptions.merge())
+            .addOnSuccessListener {
+                activity.userRegistrationSuccess()
+            }
+            .addOnFailureListener{
+            }
+    }
+
+
+}
