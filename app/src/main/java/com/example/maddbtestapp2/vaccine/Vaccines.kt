f77f0cd18eb3395ae0580f124f7ccf451ab0028e
Sara Paula <276485@student.pwr.edu.pl>
2024-05-10 01:30:43 +0200

    tylko 2 się zrobiły

--- app/src/main/java/com/example/maddbtestapp2/vaccine/Vaccines.kt
+++ app/src/main/java/com/example/maddbtestapp2/vaccine/Vaccines.kt
@@ -3,6 +3,6 @@ package com.example.maddbtestapp2.vaccine
 data class Vaccines(
     var id: Int? = null,
     var vaccineName: String,
-    var administeredDate: java.util.Date,
-    var nextDoseDate: java.util.Date
+    var administeredDate: java.sql.Date,
+    var nextDoseDate: java.sql.Date
 )
